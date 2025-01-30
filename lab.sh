9068cae3ef67fe65d11929aaf0e74b1355ce4431
pigazuco <45468172+pigazuco@users.noreply.github.com>
2025-01-30 13:17:37 +0100

    .

--- lab.sh
+++ lab.sh
@@ -1,4 +1,5 @@
-
+# Create pods in namespace default
+kubectl create -f centos.yaml
 
 
 # Create namespace and annotations for account-1
@@ -7,18 +8,4 @@ kubectl annotate namespace account-1 opflex.cisco.com/endpoint-group='{"tenant":
 
 # Deploy 3 alpine pods in account-1
 kubectl create -f alpine_acc1.yaml
-
-
-
-# Create namespace and annotations for account-2
-kubectl create namespace account-2
-kubectl annotate namespace account-2 opflex.cisco.com/endpoint-group='{"tenant":"OSP_TENANT","app-profile":"ACCOUNT_2","name":"ACCOUNT_2"}'
-
-# Deploy 3 alpine pods in account-2
-kubectl create -f alpine_acc2.yaml
-
-
-
-
-
 
